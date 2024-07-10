8ea194ce3953e735a3955d570aca1a6c50158e5e
norbert skwierczynski <norbert.fe.dev@gmail.com>
2024-07-10 22:04:14 +0200

    variables and how to write them in ts

---
+++ variables.js
@@ -0,0 +1,6 @@
+var movieTitle = "Inception";
+movieTitle = "The Dark Knight";
+movieTitle = 10; // Error: Type 'number' is not assignable to type 'string'.
+var age = 30;
+age = "30"; // Error: Type 'string' is not assignable to type 'number'.
+age += 1; // 31 Can do increment operation
---
+++ variables.ts
@@ -0,0 +1,10 @@
+let movieTitle: string = "Inception";
+movieTitle = "The Dark Knight";
+
+movieTitle = 10; // Error: Type 'number' is not assignable to type 'string'.
+
+let age: number = 30;
+
+age = "30"; // Error: Type 'string' is not assignable to type 'number'.
+
+age += 1; // 31 Can do increment operation
