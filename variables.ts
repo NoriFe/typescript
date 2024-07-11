18823dd39c317c23c001795ddff10cbf72978b7a
norbert skwierczynski <norbert.fe.dev@gmail.com>
2024-07-11 20:10:59 +0200

    Type inference in TS

--- variables.ts
+++ variables.ts
@@ -8,3 +8,8 @@ let age: number = 30;
 age = "30"; // Error: Type 'string' is not assignable to type 'number'.
 
 age += 1; // 31 Can do increment operation
+
+// type inference
+
+let movieName = "Inception"; // Type is inferred as string
+movieName = 10; // Error: Type 'number' is not assignable to type 'string'.
